64a11266c14058b3813a4ca34ace8fe5f8e323fc
jogasurya <107780867+jogasurya@users.noreply.github.com>
2023-04-04 09:47:19 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h2> How To Create Respansive Web Page</h2>
+      <div class="webscreen">Webscreen</div>
+      <div class="desktop">Desktop</div>
+      <div class="tablet">Tablet</div>
+      <div class="mobilephone">MobilePhone</div>
+    </div>
+  );
+}
